879d7052388828e1e966be32fe355eab25106996
Dylan Mooers <dmooers@calpoly.edu>
2019-04-05 11:18:05 -0700

    First submission

--- lab1.py
+++ lab1.py
@@ -2,14 +2,41 @@
 def max_list_iter(int_list):  # must use iteration not recursion
    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+   if int_list == None:
+       raise ValueError
+   max_int = None
+   for i in range(len(int_list)):
+       if i == 0 or int_list[i] > max_int:
+           max_int = int_list[i]
+
+   return max_int
 
 def reverse_rec(int_list):   # must use recursion
    """recursively reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
-   pass
+   if int_list == None:
+       raise ValueError
+   length = len(int_list)
+   if length <= 0:
+       return int_list
+   return int_list[length - 1:] + reverse_rec(int_list[:length - 1])
 
 def bin_search(target, low, high, int_list):  # must use recursion
    """searches for target in int_list[low..high] and returns index if found
    If target is not found returns None. If list is None, raises ValueError """
-   pass
+   if int_list == None:
+       raise ValueError
+   length = len(int_list)
+   if length <= 0 or target > int_list[high] or target < int_list[low]:
+       return None
+
+   middle = ((high + low) // 2)
+   vals = [int_list[low], int_list[middle], int_list[high]]
+   if target in vals:
+       return [low, middle, high][vals.index(target)]
+
+   else:# target not in [int_list[low], int_list[high], int_list[middle]]:
+       if target > int_list[middle]:
+           return bin_search(target, middle, high, int_list)
+       else:
+           return bin_search(target, low, middle, int_list)
--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -1,23 +1,51 @@
-aimport unittest
+import unittest
 from lab1 import *
 
- # A few test cases.  Add more!!!
 class TestLab1(unittest.TestCase):
 
     def test_max_list_iter(self):
-        """add description here"""
+        """Returns max value in a populated list, None if empty and raises ValueError if argument is Nonetype"""
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
+        
+        #This tests for a large list
+        lista = [i for i in range(1000)]
+        self.assertEqual(max_list_iter(lista), 999)
+        
+        #This tests for a list of length 1
+        self.assertEqual(max_list_iter([1]), 1)
+        
+        #This tests for a list with a repeating number
+        self.assertEqual(max_list_iter([1, 3, 3]), 3)
+        
+        #This tests for an empty list returning None
+        self.assertEqual(max_list_iter([]), None)
+
 
     def test_reverse_rec(self):
-        self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
+        self.assertEqual(reverse_rec([1,2,3]),[3,2,1]) #Used to test basic functionality
+        self.assertEqual(reverse_rec([4, 7, 6, 3, 9]), [9, 3, 6, 7, 4]) #Checks functionality when numbers arent in order
+        self.assertEqual(reverse_rec([]), []) #Checks empty list return
+
+        with self.assertRaises(ValueError): #Checks if ValueError is returned
+            reverse_rec(None)
 
     def test_bin_search(self):
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
         high = len(list_val)-1
-        self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
+        self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 ) # Checks for basic functionality
+
+        list_val1 = []
+        self.assertEqual(bin_search(5, 2, 10, list_val1), None) #Checks for searching an empty list
+        
+        big_list = [i for i in range(100)]
+        self.assertEqual(bin_search(100, 0, 99, big_list), None) #Checks big list for value not present
+        self.assertEqual(bin_search(9, 0, 99, big_list), 9) #Checks big list for value that is present
+
+        with self.assertRaises(ValueError): #Checks for ValueError if int_list is NoneType
+            bin_search(4, 0, 10, None)
 
 if __name__ == "__main__":
         unittest.main()
