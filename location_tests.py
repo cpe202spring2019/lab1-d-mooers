bafc324a1147aed365dd656661daa16ee2881f68
Dylan Mooers <dmooers@calpoly.edu>
2019-04-05 11:18:29 -0700

    First submission

--- location_tests.py
+++ location_tests.py
@@ -6,6 +6,13 @@ class TestLab1(unittest.TestCase):
     def test_repr(self):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
+
+    def test_eq(self):
+        loc1 = Location("PIE", 3.14, 4.13)
+        loc2 = Location("Olympia", 222, 218)
+        loc3 = Location("PIE", 3.14, 4.13)
+        self.assertTrue(loc1 == loc3)
+        self.assertFalse(loc1 == loc2)
     
     # Add more tests!
 
