af3775ffce7f9c6eae0a2e65c916f0f7485e7aa5
Dylan Mooers <dmooers@calpoly.edu>
2019-04-10 19:46:28 -0700

    Fixing how binary search operates w a list w len of 1

--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -9,26 +9,34 @@ class TestLab1(unittest.TestCase):
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
         
-        #This tests for a large list
+        # This tests for a large list
         lista = [i for i in range(1000)]
         self.assertEqual(max_list_iter(lista), 999)
         
-        #This tests for a list of length 1
+        # This tests for a list of length 1
         self.assertEqual(max_list_iter([1]), 1)
         
-        #This tests for a list with a repeating number
+        # This tests for a list with a repeating number
         self.assertEqual(max_list_iter([1, 3, 3]), 3)
         
-        #This tests for an empty list returning None
+        # This tests for an empty list returning None
         self.assertEqual(max_list_iter([]), None)
+
+        # This tests for a list with a repeating number
+        self.assertEqual(max_list_iter([3, 3, 3]), 3)
+
+        # This tests for a list with a repeating number
+        self.assertEqual(max_list_iter([3, 3, 4]), 4)
 
 
     def test_reverse_rec(self):
-        self.assertEqual(reverse_rec([1,2,3]),[3,2,1]) #Used to test basic functionality
-        self.assertEqual(reverse_rec([4, 7, 6, 3, 9]), [9, 3, 6, 7, 4]) #Checks functionality when numbers arent in order
-        self.assertEqual(reverse_rec([]), []) #Checks empty list return
+        self.assertEqual(reverse_rec([1,2,3]),[3,2,1]) # Used to test basic functionality
+        self.assertEqual(reverse_rec([4, 7, 6, 3, 9]), [9, 3, 6, 7, 4]) # Checks functionality when numbers arent in order
+        self.assertEqual(reverse_rec([]), []) # Checks empty list return
 
-        with self.assertRaises(ValueError): #Checks if ValueError is returned
+        self.assertEqual(reverse_rec([1]), [1])  # Checks list with only one entry
+
+        with self.assertRaises(ValueError):  # Checks if ValueError is returned
             reverse_rec(None)
 
     def test_bin_search(self):
@@ -38,15 +46,22 @@ class TestLab1(unittest.TestCase):
         self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 ) # Checks for basic functionality
 
         list_val1 = []
-        self.assertEqual(bin_search(5, 2, 10, list_val1), None) #Checks for searching an empty list
+        self.assertEqual(bin_search(5, 0, 9, list_val1), None)  # Checks for searching an empty list
         
-        self.assertEqual(bin_search(8, 0, 4, [2, 5, 8, 90, 123]), 2) #Tests with a list that isnt just "1, 2, 3, 4, ..."
+        self.assertEqual(bin_search(8, 0, 4, [2, 5, 8, 90, 123]), 2)  # Tests with a complex list"
 
         big_list = [i for i in range(100)]
-        self.assertEqual(bin_search(100, 0, 99, big_list), None) #Checks big list for value not present
-        self.assertEqual(bin_search(9, 0, 99, big_list), 9) #Checks big list for value that is present
+        self.assertEqual(bin_search(100, 0, 99, big_list), None)  # Checks big list for value not present
+        self.assertEqual(bin_search(9, 0, 99, big_list), 9)  # Checks big list for value that is present
 
-        with self.assertRaises(ValueError): #Checks for ValueError if int_list is NoneType
+        self.assertEqual(bin_search(-1, 0, 99, big_list), None)  # Checks big list for value less than first indices
+
+        self.assertEqual(bin_search(2, 0, 0, [2]), 0)  # Checks single entry list
+
+        self.assertEqual(bin_search(3, 0, 3, [3, 4, 7, 12]), 0)  # Checks low index
+        self.assertEqual(bin_search(12, 0, 3, [3, 4, 7, 12]), 3)  # Checks high index
+
+        with self.assertRaises(ValueError):  # Checks for ValueError if int_list is NoneType
             bin_search(4, 0, 10, None)
 
 if __name__ == "__main__":
